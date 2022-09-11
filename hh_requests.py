faa3f925e864a5612633064f4c0648dfb3336cf8
tihhor <tihhor@gmail.com>
2022-09-11 12:41:32 +0200

    dz13

--- hh_requests.py
+++ hh_requests.py
@@ -1,82 +1,79 @@
-
 import requests
 import pprint
 import time
 import json
 
-url = 'https://api.hh.ru/vacancies'
 
-request_text = input('Введите запрос для поиска вакансий:')
-if request_text == '':
-    request_text = 'Python AND Django'
+# request_text = input('Введите запрос для поиска вакансий:')
+request_text = ''
 
-key_skills = {}
-vacancies_total = 0     #число найденных вакансий
-salary_total = 0        #суммарная зарплата вакансий - для расчета средней
-NUM_PAGES = 2           #MAX 20    #ограничение числа страниц поиска
+def  hh_search(request_text):
+    url = 'https://api.hh.ru/vacancies'
 
-for page_number in range(NUM_PAGES):
+    if request_text == '':
+        request_text = 'Python AND Django'
 
-    parameters = {
-         'text': 'NAME:'+request_text,
-         'per_page': 20,
-         'page': page_number,
-         'only_with_salary': True,
-         'currency': 'RUR'
-    }
+    key_skills = {}
+    vacancies_total = 0     #число найденных вакансий
+    salary_total = 0        #суммарная зарплата вакансий - для расчета средней
+    NUM_PAGES = 2           #MAX 20    #ограничение числа страниц поиска
 
-    result = requests.get(url, params=parameters).json()
+    for page_number in range(NUM_PAGES):
 
-    # print('Страница:', page_number)
-    # pprint.pprint(result)
+        parameters = {
+             'text': 'NAME:'+request_text,
+             'per_page': 10,
+             'page': page_number,
+             'only_with_salary': True,
+             'currency': 'RUR'
+        }
 
-    vacancies = result['items']     #получили список вакансий
+        result = requests.get(url, params=parameters).json()
 
-    for vacancy in vacancies:
-        url_vacancy = vacancy['url']
-        result = requests.get(url_vacancy).json()   #запрашиваем данные по каждой вакансии
-        salary = result['salary']
+        # print('Страница:', page_number)
+        # pprint.pprint(result)
 
-        if salary['currency'] == 'RUR' and not (salary['from'] is None and salary['to'] is None) :    #учитываем только вакансии с указанной зарплатой в рублях
-            # print(result['key_skills'])
-            # print(result['salary'])
-            vacancies_total  += 1
-            # вычисляем зарплату как среднее между верхним и нижним значением
-            salary_start = 0 if salary['from'] is None else salary['from']
-            salary_finish = salary_start if salary['to'] is None else salary['to']
-            salary_total  += (salary_start+salary_finish)/2
+        vacancies = result['items']     #получили список вакансий
 
-            skills = result['key_skills']   #разбираем ключевые навыки
-            for skill in skills:
-                item = skill['name']
-                if item in key_skills:
-                    key_skills[item] += 1
-                else:
-                    key_skills[item] = 1
+        for vacancy in vacancies:
+            url_vacancy = vacancy['url']
+            result = requests.get(url_vacancy).json()   #запрашиваем данные по каждой вакансии
+            salary = result['salary']
 
-            time.sleep(1)
+            if salary['currency'] == 'RUR' and not (salary['from'] is None and salary['to'] is None) :    #учитываем только вакансии с указанной зарплатой в рублях
+                # print(result['key_skills'])
+                # print(result['salary'])
+                vacancies_total  += 1
+                # вычисляем зарплату как среднее между верхним и нижним значением
+                salary_start = 0 if salary['from'] is None else salary['from']
+                salary_finish = salary_start if salary['to'] is None else salary['to']
+                salary_total  += (salary_start+salary_finish)/2
 
-#print(key_skills)
+                skills = result['key_skills']   #разбираем ключевые навыки
+                for skill in skills:
+                    item = skill['name']
+                    if item in key_skills:
+                        key_skills[item] += 1
+                    else:
+                        key_skills[item] = 1
 
-key_skills_sorted = sorted(key_skills.items(), key=lambda x: x[1], reverse=True)
+                time.sleep(1)
 
-request_result = {}      #записвываем результаты в словарь
+    #print(key_skills)
 
-request_result['request_text'] = request_text
-request_result['vacancies_total'] = vacancies_total
-request_result['average_salary'] = round(salary_total/vacancies_total,-3)
-request_result['key_skills'] = key_skills_sorted
+    key_skills_sorted = sorted(key_skills.items(), key=lambda x: x[1], reverse=True)
 
-print('ПАРАМЕТРЫ ЗАПРОСА:', request_text)
-print('ВСЕГО ВАКАНСИЙ:', vacancies_total)
-print('СРЕДНЯЯ ЗАРПЛАТА:', request_result['average_salary'])
-print('СПИСОК КЛЮЧЕВЫХ НАВЫКОВ:')
-for item in key_skills_sorted:
-    print(f'{item[0]} {item[1]}  {round(item[1]/vacancies_total*100)} %' )
+    request_result = {}      #записвываем результаты в словарь
 
+    request_result['request_text'] = request_text
+    request_result['vacancies_total'] = vacancies_total
+    request_result['average_salary'] = 0 if salary_total == 0 else round(salary_total/vacancies_total,-3)
+    request_result['key_skills'] = key_skills_sorted[:3]
 
-#сохраняем словарь результатов в файл .json
-with open('request_result.json', "w", encoding="utf-8") as file:
-    json.dump(request_result, file)
+    #сохраняем словарь результатов в файл .json
+    with open('request_result.json', "w", encoding="utf-8") as file:
+        json.dump(request_result, file)
 
-print('Результат сохранен в файле request_result.json')
+    print('Результат сохранен в файле request_result.json')
+
+    return (request_result)
